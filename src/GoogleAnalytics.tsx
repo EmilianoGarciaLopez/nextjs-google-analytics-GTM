383e1124a9adff5d77258a1177712d74bdb674fe
Emiliano García-López <emilianogarcialopez26@gmail.com>
2022-03-01 08:26:09 -0500

    updated deps

--- src/GoogleAnalytics.tsx
+++ src/GoogleAnalytics.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export function GoogleAnalytics(): JSX.Element | null {
-  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  const gtmDomain = process.env.NEXT_PUBLIC_GTM_DOMAIN;
+const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const gtmDomain = process.env.NEXT_PUBLIC_GTM_DOMAIN;
 
+export function GoogleAnalytics(): JSX.Element | null {
   if (!gaMeasurementId) {
     return null;
   }
@@ -30,5 +30,22 @@ export function GoogleAnalytics(): JSX.Element | null {
         }}
       />
     </>
+  );
+}
+
+export function GoogleAnalyticsNoscript(): JSX.Element | null {
+  if (!gaMeasurementId) {
+    return null;
+  }
+
+  return (
+    <noscript>
+      <iframe
+        src={`https://${gtmDomain}/ns.html?id=${gaMeasurementId}`}
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
   );
 }
